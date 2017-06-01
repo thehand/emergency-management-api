6c48c5e0f103ec262d595ba2f8235ae2165eb034
Francesco Salamone <salamone.francesco@gmail.com>
2017-06-01 11:29:44 +0200

    Add Wallets Class

---
+++ src/WalletLogger/Wallets.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace WalletLogger;
+
+class Wallets
+{
+    public function listItems()
+    {
+        return array(
+            array(
+                'id' => 1
+            ),
+            array(
+                'id' => 2
+            ),
+            array(
+                'id' => 3
+            ),
+        );
+    }
+}
