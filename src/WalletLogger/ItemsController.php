ae69be799c5e3939615bdb8f001ee2e37f4dd947
Francesco Salamone <salamone.francesco@gmail.com>
2017-06-01 20:19:15 +0200

    Complete basic items methods, list, create, get, update and delete

--- src/WalletLogger/ItemsController.php
+++ src/WalletLogger/ItemsController.php
@@ -20,46 +20,124 @@ class ItemsController implements ItemsInterface
 
     public function listItems(Request $request, Response $response, Array $args)
     {
-        // TODO: Check if item exists, if so, return it
-        return $response->withStatus(200)->withJson($this->table->orderBy($this->order_by)->get()->where('deleted_at',null));
+        $output = [
+            'status' => 204,
+            'message' => 'No Content',
+            'total_items' => 0,
+            'items' => null,
+        ];
+
+        $items = $this->table->orderBy($this->order_by)->get()->where('deleted_at', null);
+        if (count($items) > 0) {
+            $output = [
+                'status' => 200,
+                'message' => 'OK',
+                'total_items' => count($items),
+                'items' => $items,
+            ];
+        }
+
+        return $response->withStatus($output['status'])->withJson($output);
     }
 
     public function getItem(Request $request, Response $response, Array $args)
     {
-        // TODO: Check if item was deleted and if exists, return it
-        return $response->withStatus(200)->withJson($this->table->find($args['id']));
+        $output = [
+            'status' => 404,
+            'message' => 'Not Found',
+            'total_items' => 0,
+            'item' => null,
+        ];
+
+        $item = $this->table->find($args['id']);
+        if ($item->id > 0 && null === $item->deleted_at) {
+            $output = [
+                'status' => 200,
+                'message' => 'OK',
+                'total_items' => 1,
+                'item' => $item,
+            ];
+        }
+
+        return $response->withStatus($output['status'])->withJson($output);
     }
 
     public function createItem(Request $request, Response $response, Array $args)
     {
         $post_data = $request->getParsedBody();
-        $item = $this->table->insert($post_data);
+        $output = [
+            'status' => 500,
+            'message' => 'Internal Server Error',
+            'total_items' => 0,
+            'item' => null,
+        ];
 
-        return $response->withStatus(200)->withJson($item);
+        try {
+            // TODO: Check if for that user, a wallet with specified name already exists
+            $item = $this->table->insertGetId($post_data);
+
+            return $this->getItem($request, $response, ['id' => $item]);
+        } catch (\Exception $exception) {
+            // TODO: Maybe log errors?
+            return $response->withStatus($output['status'])->withJson($output);
+        }
     }
 
     public function updateItem(Request $request, Response $response, Array $args)
     {
-        $update = false;
-        $post_data = $request->getParsedBody();
-
-        $item = $this->table->find($args['id']);
-        if ($item) {
-            $update = $this->table->update($post_data);
+        try {
+            $update = $this->updateDBRow($args['id'], $request->getParsedBody());
+            if ($update) {
+                return $this->getItem($request, $response, $args);
+            }
+        } catch (\Exception $exception) {
+            // TODO: Maybe log errors?
+            $output = [
+                'status' => 500,
+                'message' => 'Internal Server Error',
+                'total_items' => 0,
+                'item' => null,
+            ];
+            return $response->withStatus(500)->withJson($output);
         }
-
-        return $response->withStatus(200)->withJson($update);
     }
 
     public function deleteItem(Request $request, Response $response, Array $args)
     {
-        $update = false;
+        try {
+            $delete = $this->updateDBRow($args['id'], ['deleted_at' => date('Y-m-d H:i:s')]);
+            if ($delete) {
+                $output = [
+                    'status' => 200,
+                    'message' => 'OK',
+                    'total_items' => 0,
+                    'item' => null,
+                ];
 
-        $item = $this->table->find($args['id']);
-        if ($item) {
-            $update = $this->table->update(['deleted_at' => date('Y-m-d H:i:s')]);
+                return $response->withStatus(200)->withJson($output);
+            }
+        } catch (\Exception $exception) {
+            $output = [
+                'status' => 500,
+                'message' => 'Internal Server Error',
+                'total_items' => 0,
+                'item' => null,
+            ];
+            return $response->withStatus(500)->withJson($output);
         }
+    }
 
-        return $response->withStatus(200)->withJson($update);
+    public function updateDBRow($item_id, Array $updated_data)
+    {
+        $item = $this->table->find((int)$item_id);
+        if ($item) {
+            try {
+                return $this->table->update($updated_data);
+            } catch (\Exception $exception) {
+                // TODO: Maybe log errors?
+                return false;
+            }
+
+        }
     }
 }
