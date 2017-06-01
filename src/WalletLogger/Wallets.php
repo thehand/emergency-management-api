407611203aed7c4377523925e822ccd2d4dc33cd
Francesco Salamone <salamone.francesco@gmail.com>
2017-06-01 14:27:02 +0200

    Clear old Wallets Class

--- src/WalletLogger/Wallets.php
+++ src/WalletLogger/Wallets.php
@@ -4,18 +4,4 @@ namespace WalletLogger;
 
 class Wallets
 {
-    public function listItems()
-    {
-        return array(
-            array(
-                'id' => 1
-            ),
-            array(
-                'id' => 2
-            ),
-            array(
-                'id' => 3
-            ),
-        );
-    }
 }
