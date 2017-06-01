47340ce1026fda72c248e38c3c996c64d185d81c
Francesco Salamone <salamone.francesco@gmail.com>
2017-06-01 15:23:54 +0200

    Update ItemsInterface

--- src/WalletLogger/ItemsInterface.php
+++ src/WalletLogger/ItemsInterface.php
@@ -2,11 +2,14 @@
 
 namespace WalletLogger;
 
+use Illuminate\Database\Query\Builder;
 use Slim\Http\Request;
 use Slim\Http\Response;
 
 interface ItemsInterface
 {
+    public function __construct(Builder $table);
+
     public function getItem(Request $request, Response $response, Array $args);
 
     public function createItem();
