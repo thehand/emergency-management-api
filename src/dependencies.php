289c162526dbcf18683f6719b24644f594170831
Francesco Salamone <salamone.francesco@gmail.com>
2017-06-01 15:22:44 +0200

    Inject Eloquent into Slim

--- src/dependencies.php
+++ src/dependencies.php
@@ -2,6 +2,18 @@
 
 $container = $app->getContainer();
 
-$container['wallets'] = function () {
-    return new \WalletLogger\Wallets();
+$container['db'] = function (\Slim\Container $c) {
+    $capsule = new \Illuminate\Database\Capsule\Manager;
+    $capsule->addConnection($c->get('dbConnection'));
+
+    $capsule->setAsGlobal();
+    $capsule->bootEloquent();
+
+    return $capsule;
+};
+
+$container[\WalletLogger\WalletsController::class] = function (\Slim\Container $c) {
+    $table = $c->get('db')->table('wallets');
+
+    return new \WalletLogger\WalletsController($table);
 };
