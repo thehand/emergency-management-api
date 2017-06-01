af6da081f7d0916c1b59aeafce93f4c8e99c8759
Francesco Salamone <salamone.francesco@gmail.com>
2017-06-01 09:58:17 +0200

    Update index registering settings and routes

--- src/public/index.php
+++ src/public/index.php
@@ -3,7 +3,13 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-require '../vendor/autoload.php';
-require '../settings.php';
+require __DIR__ . '/../vendor/autoload.php';
 
+require __DIR__ . '/../settings.php';
 $app = new \Slim\App($settings);
+
+// Register routes
+require __DIR__ . '/../routes.php';
+
+// Run
+$app->run();
