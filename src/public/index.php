7cc74d5559b97cb405181679f2733dd6c2250cde
Francesco Salamone <salamone.francesco@gmail.com>
2017-06-01 14:22:32 +0200

    Clean up index file

--- src/public/index.php
+++ src/public/index.php
@@ -1,19 +1,17 @@
 <?php
 
-use \Psr\Http\Message\ServerRequestInterface as Request;
-use \Psr\Http\Message\ResponseInterface as Response;
-use \WalletLogger\Wallets;
-
 require __DIR__ . '/../vendor/autoload.php';
 
-// Todo: If settings.php file does not exists throw an exception and warn users
+// TODO: If settings.php file does not exists throw an exception and warn users
 require __DIR__ . '/../settings.php';
 $app = new \Slim\App($settings);
 
 // Register dependencies
+// TODO: If dependencies.php file does not exists throw an exception and warn users
 require __DIR__ . '/../dependencies.php';
 
 // Register routes
+// TODO: If routes.php file does not exists throw an exception and warn users
 require __DIR__ . '/../routes.php';
 
 // Run
