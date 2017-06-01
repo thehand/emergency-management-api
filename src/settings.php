d4d5294ec282e82a92f9a408fe4b7382912732c0
Francesco Salamone <salamone.francesco@gmail.com>
2017-06-01 09:23:16 +0200

    Insert DB configuration to settings.php

--- src/settings.php
+++ src/settings.php
@@ -3,4 +3,11 @@ date_default_timezone_set('Europe/Rome');
 
 $settings = array(
     'displayErrorDetails' => true,
+
+    'db' => array(
+        'host' => 'CHOOSE_YOUR_SOURCE_HOST',
+        'user' => 'CHOOSE_YOUR_DB_USER',
+        'pass' => 'CHOOSE_YOUR_DB_PASSWORD',
+        'dbname' => 'wallet_logger',
+    )
 );
