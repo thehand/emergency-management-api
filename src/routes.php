1857f9152ddd7dad04dd474fa296350ea56de5fe
Francesco Salamone <salamone.francesco@gmail.com>
2017-06-01 09:57:38 +0200

    Add an idea of needed routes

---
+++ src/routes.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * Wallets routes
+ */
+$app->group('/wallets', function () {
+    $this->get('', function ($request, $response, $args) {
+        // TODO: Use db to retrieve the list of wallets
+    });
+    $this->post('', function ($request, $response, $args) {
+        // TODO: Use db to create a new wallet
+    });
+    $this->patch('/{id:[0-9]+}', function ($request, $response, $args) {
+        // TODO: Use db to update a wallet
+    });
+    $this->delete('/{id:[0-9]+}', function ($request, $response, $args) {
+        // TODO: Use db to delete a wallet
+    });
+});
+
+
+/**
+ * Accounts routes
+ */
+$app->group('/accounts', function () {
+    $this->get('', function ($request, $response, $args) {
+        // TODO: Use db to retrieve the list of accounts
+    });
+    $this->post('', function ($request, $response, $args) {
+        // TODO: Use db to create a new account
+    });
+    $this->patch('/{id:[0-9]+}', function ($request, $response, $args) {
+        // TODO: Use db to update a account
+    });
+    $this->delete('/{id:[0-9]+}', function ($request, $response, $args) {
+        // TODO: Use db to delete a account
+    });
+});
+
+
+/**
+ * Transactions routes
+ */
+$app->group('/transactions', function () {
+    $this->get('', function ($request, $response, $args) {
+        // TODO: Use db to retrieve the list of transactions
+    });
+    $this->post('', function ($request, $response, $args) {
+        // TODO: Use db to create a new transaction
+    });
+    $this->patch('/{id:[0-9]+}', function ($request, $response, $args) {
+        // TODO: Use db to update a transaction
+    });
+    $this->delete('/{id:[0-9]+}', function ($request, $response, $args) {
+        // TODO: Use db to delete a transaction
+    });
+});
