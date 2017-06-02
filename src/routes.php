bf4b28c10a36d0e0b45d19b6dd32295f8053856b
Francesco Salamone <salamone.francesco@gmail.com>
2017-06-02 17:21:23 +0200

    Cleanup routes file

--- src/routes.php
+++ src/routes.php
@@ -1,19 +1,4 @@
 <?php
-/**
- * Register specific routes for each available item types
- */
-/*
-foreach ($app->getContainer()['item_types'] as $item_type) {
-    $app->group('/' . $item_type, function () use ($item_type) {
-        $this->get('', 'WalletLogger\\' . ucfirst($item_type) . 'Controller:listItems')->setName('getList' . ucfirst($item_type)); // List all items
-        $this->get('/{id:[0-9]+}', 'WalletLogger\\' . ucfirst($item_type) . 'Controller:getItem')->setName('getItem' . ucfirst($item_type)); // Get item's data
-        $this->post('', 'WalletLogger\\' . ucfirst($item_type) . 'Controller:createItem'); // Create a new item
-        $this->post('/{id:[0-9]+}', 'WalletLogger\\' . ucfirst($item_type) . 'Controller:updateItem'); // Update an item
-        // Should be more correct use PATCH verb to update an item but Slim seems to have problems with getParsedBody method :(
-        $this->delete('/{id:[0-9]+}', 'WalletLogger\\' . ucfirst($item_type) . 'Controller:deleteItem'); // Delete an item
-    });
-}
-*/
 
 /**
  * Register routes to follow the hierarchy of items
@@ -32,7 +17,7 @@ $app->group('/wallets', function () use ($app) {
         $this->post('/{id:[0-9]+}', 'WalletLogger\AccountsController:updateItem'); // Update a account
         $this->delete('/{id:[0-9]+}', 'WalletLogger\AccountsController:deleteItem'); // Delete a account
 
-        $app->group('/{fk_account_id:[0-9]+}/transactions', function () use ($app) {
+        $app->group('/{fk_account_id:[0-9]+}/transactions', function () {
             $this->get('', 'WalletLogger\TransactionsController:listItems'); // List all transactions
             $this->get('/{id:[0-9]+}', 'WalletLogger\TransactionsController:getItem'); // Get a transaction
             $this->post('', 'WalletLogger\TransactionsController:createItem'); // Create a new transaction
