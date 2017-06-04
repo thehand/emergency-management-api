71dc27a06e4a81e8fc41110dfdceff34246da3e6
Francesco Salamone <salamone.francesco@gmail.com>
2017-06-04 20:16:10 +0200

    Add Model Interface

---
+++ src/WalletLogger/Interfaces/ItemsModelInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace WalletLogger\Interfaces;
+
+use Illuminate\Database\Capsule\Manager;
+
+interface ItemsModelInterface
+{
+    public function __construct(Manager $db);
+
+    public function getList(array $filters, $order_by = 'id');
+
+    public function getItem($id);
+
+    public function createItem($item_data);
+
+    public function updateItem($item_id, Array $updated_data);
+}
