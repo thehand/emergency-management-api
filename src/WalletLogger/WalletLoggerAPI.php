cda06d0d63423bbd2d390f8188ffbdce49a16a41
Francesco Salamone <salamone.francesco@gmail.com>
2017-06-08 20:42:22 +0200

    Update main class to implement users authentication

--- src/WalletLogger/WalletLoggerAPI.php
+++ src/WalletLogger/WalletLoggerAPI.php
@@ -2,37 +2,90 @@
 
 namespace WalletLogger;
 
+use Illuminate\Database\Capsule\Manager;
 use Slim\App as Slim;
+use Slim\Container;
+use Slim\Http\Request;
+use Slim\Middleware\TokenAuthentication;
 
 class WalletLoggerAPI
 {
+    /** @var Slim $app */
     private $app;
+
+    /** @var UsersController $this->authenticator */
+    public $authenticator;
 
     public function __construct()
     {
+        $this->registerSettings();
+        $this->registerDependencies();
+
+        $this->authenticator = function (Request $request, TokenAuthentication $tokenAuth) {
+            $token = $tokenAuth->findToken($request);
+
+            $user = (new UsersController($this->app->getContainer()['db']))->authToken($token);
+
+            if (!$user) {
+                throw new UnauthorizedException('Authentication Failed');
+            }
+        };
+
+        $this->registerRouter();
+    }
+
+    private function registerSettings()
+    {
         $settings = [];
+
         // Load settings
         if (file_exists(__DIR__ . '/../settings.php')) {
             require __DIR__ . '/../settings.php';
         } else {
             throw new \RuntimeException('Settings file is missing.');
         }
-        $app = new Slim($settings);
 
-        // Register dependencies
-        if (file_exists(__DIR__ . '/../dependencies.php')) {
-            require __DIR__ . '/../dependencies.php';
-        } else {
-            throw new \RuntimeException('Dependencies file is missing.');
-        }
+        $this->app = new Slim($settings);
+    }
 
+    private function registerDependencies()
+    {
+        $this->app->getContainer()['db'] = function (Container $c) {
+            $capsule = new Manager();
+            $capsule->addConnection($c->get('dbConnection'));
+
+            $capsule->setAsGlobal();
+            $capsule->bootEloquent();
+
+            return $capsule;
+        };
+
+        $this->app->getContainer()[UsersController::class] = function (Container $c) {
+            return new UsersController($c->get('db'));
+        };
+
+        $this->app->getContainer()[WalletsController::class] = function (Container $c) {
+            return new WalletsController($c->get('db'));
+        };
+
+        $this->app->getContainer()[AccountsController::class] = function (Container $c) {
+            return new AccountsController($c->get('db'));
+        };
+
+        $this->app->getContainer()[TransactionsController::class] = function (Container $c) {
+            return new TransactionsController($c->get('db'));
+        };
+    }
+
+    private function registerRouter()
+    {
         // Register routes
+        $app = $this->get();
         if (file_exists(__DIR__ . '/../routes.php')) {
             require __DIR__ . '/../routes.php';
         } else {
             throw new \RuntimeException('Routes file is missing.');
         }
-
         $this->app = $app;
     }
 
