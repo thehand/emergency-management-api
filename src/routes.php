ca0e76397faef9dc75cd56d7fae5380e90922803
Francesco Salamone <salamone.francesco@gmail.com>
2017-06-09 16:24:39 +0200

    Update fallback route

--- src/routes.php
+++ src/routes.php
@@ -21,7 +21,6 @@ $app->group('/wallets', function () use ($app) {
     $this->get('/{id:[0-9]+}', 'WalletLogger\WalletsController:getItem'); // Get a wallet
     $this->post('', 'WalletLogger\WalletsController:createItem'); // Create a new wallet
     $this->post('/{id:[0-9]+}', 'WalletLogger\WalletsController:updateItem'); // Update a wallet
-    $this->options('/{id:[0-9]+}', 'WalletLogger\WalletsController:getItem'); // Enable options method
     $this->delete('/{id:[0-9]+}', 'WalletLogger\WalletsController:deleteItem'); // Delete a wallet
 
     $app->group('/{fk_wallet_id:[0-9]+}/accounts', function () use ($app) {
@@ -29,7 +28,6 @@ $app->group('/wallets', function () use ($app) {
         $this->get('/{id:[0-9]+}', 'WalletLogger\AccountsController:getItem'); // Get an account
         $this->post('', 'WalletLogger\AccountsController:createItem'); // Create a new account
         $this->post('/{id:[0-9]+}', 'WalletLogger\AccountsController:updateItem'); // Update a account
-        $this->options('/{id:[0-9]+}', 'WalletLogger\AccountsController:getItem'); // Enable options method
         $this->delete('/{id:[0-9]+}', 'WalletLogger\AccountsController:deleteItem'); // Delete a account
 
         $app->group('/{fk_account_id:[0-9]+}/transactions', function () {
@@ -37,7 +35,6 @@ $app->group('/wallets', function () use ($app) {
             $this->get('/{id:[0-9]+}', 'WalletLogger\TransactionsController:getItem'); // Get a transaction
             $this->post('', 'WalletLogger\TransactionsController:createItem'); // Create a new transaction
             $this->post('/{id:[0-9]+}', 'WalletLogger\TransactionsController:updateItem'); // Update a transaction
-            $this->options('/{id:[0-9]+}', 'WalletLogger\TransactionsController:getItem'); // Enable options method
             $this->delete('/{id:[0-9]+}', 'WalletLogger\TransactionsController:deleteItem'); // Delete a transaction
         });
     });
@@ -49,3 +46,9 @@ $app->group('/wallets', function () use ($app) {
         'secure' => false,
     ]));
 
+/**
+ * Return an empty response for every other routes
+ */
+$app->any('[/{path:.*}]', function (\Slim\Http\Request $request, \Slim\Http\Response $response, Array $args) {
+    return $response->withStatus(200);
+})->add($addCORS);
